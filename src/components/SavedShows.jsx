4363e77e796c7484f75fb9cdb6417c43a4cb78ed
William Cadiz <wcadz27@gmail.com>
2022-06-15 10:26:26 -0700

    Added backdrop in account page and the page shows favourite movies

--- src/components/SavedShows.jsx
+++ src/components/SavedShows.jsx
@@ -40,46 +40,48 @@ const SavedShows = () => {
   };
   return (
     <div
-      className="relative ml-8 group"
+      className="relative group w-full h-auto mt-8"
       /* className="flex" */
       /* className="flex relative items-center overflow-x-scroll scroll-smooth scrollbar scrollbar-hide group" */
     >
       <MdChevronLeft
         onClick={slideLeft}
         size={40}
-        className="bg-white rounded-full top-[130px] absolute opacity-50 hover:opacity-100 cursor-pointer z-10 group-hover:block hidden md:top-[50px]"
+        className="bg-white top-[30%] rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 group-hover:block hidden"
       />
       <div
         id={"slider"}
-        className="relative flex w-full flex-nowrap overflow-x-scroll scroll-smooth scrollbar-hide"
+        className="relative flex w-full h-auto overflow-x-scroll scroll-smooth scrollbar-hide"
       >
         {movies.map((item) => (
-          <div
-            key={item.id}
-            className="relative w-[160px] sm:w-[200px] md:w-[240px] lg:w-[200px] p-2"
-            /* className="flex w-full h-full cursor-pointer" */
-          >
-            <img
-              className="rounded-2xl cursor-pointer"
-              src={`https://image.tmdb.org/t/p/w500/${item?.img}`}
-              alt={item?.title}
-            />
-            <div className="flex items-center justify-center absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 rounded-2xl cursor-pointer">
-              <PlayIcon />
-              <div>
-                <AiOutlineClose
-                  onClick={() => deleteShow(item.id)}
-                  className="absolute text-gray-300 top-4 right-4"
-                />
+          <div key={item.id} className="flex relative flex-col w-full h-auto">
+            <div
+              className="relative w-[300px] h-auto p-2"
+              /* className="flex w-full h-full cursor-pointer" */
+            >
+              <img
+                className="rounded-2xl cursor-pointer h-auto w-full"
+                src={`https://image.tmdb.org/t/p/w500/${item?.img}`}
+                alt={item?.title}
+              />
+              <div className="flex items-center justify-center absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 rounded-2xl cursor-pointer">
+                <PlayIcon />
+                <div>
+                  <AiOutlineClose
+                    onClick={() => deleteShow(item.id)}
+                    className="absolute text-gray-300 top-4 right-4"
+                  />
+                </div>
               </div>
             </div>
+            <p className="text-white text-center mt-4">{item?.title}</p>
           </div>
         ))}
       </div>
       <MdChevronRight
         onClick={slideRight}
         size={40}
-        className="bg-white rounded-full absolute top-[130px] right-0 opacity-50 hover:opacity-100 cursor-pointer z-10 group-hover:block hidden md:top-[50px]"
+        className="bg-white top-[30%] rounded-full absolute right-0 opacity-50 hover:opacity-100 cursor-pointer z-10 group-hover:block hidden"
       />
     </div>
   );
--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -4,9 +4,24 @@ import SavedShows from "../components/SavedShows";
 const Account = () => {
   return (
     <>
-      <div className="absolute top-[20%]">
-        <h1 className="text-white text-3xl md:text-5xl font-bold"> My Shows</h1>
-        <SavedShows />
+      <div className="flex flex-col h-full w-full">
+        <div className="h-[350px] w-full">
+          <div className="flex items-center absolute w-full h-[350px] bg-gradient-to-r from-black">
+            {" "}
+            <h1 className="absolute text-white text-3xl md:text-4xl font-bold ml-4">
+              {" "}
+              My Shows
+            </h1>
+          </div>
+          <img
+            className="w-full h-full object-cover"
+            src="https://fandomwire.com/wp-content/uploads/2018/08/Movies-background-1068x616.png"
+            alt="movieslist"
+          />
+        </div>
+        <div className="w-full h-full">
+          <SavedShows />
+        </div>
       </div>
     </>
   );

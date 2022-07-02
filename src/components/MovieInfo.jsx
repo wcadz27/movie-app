57c028be69f2c43d8dd927d6e1cf575ce66997c4
William Cadiz <wcadz27@gmail.com>
2022-07-02 07:35:36 -0700

    Fixed responsivity of modal component up to 1023px from 375px

--- src/components/MovieInfo.jsx
+++ src/components/MovieInfo.jsx
@@ -48,41 +48,51 @@ const MovieInfo = ({
             />
           </div>
         </div>
-        <div className="hidden md:block w-[20%]">
+        <div className="hidden absolute md:block w-[20%]">
           <img
             className="w-full h-auto object-cover"
             src={`https://image.tmdb.org/t/p/original//${showModal?.poster_path}`}
             alt=""
           />
         </div>
-        <div className="absolute flex flex-col w-[95vw] ml-2 text-white top-[11rem] pb-4">
-          <div className="text-white">
-            <h1 className="text-4xl font-bold bottom-[20%]">
-              {showModal.title}
-            </h1>
-            <ul className="mt-1 text-[0.5rem]">
-              <li className="border-2 inline rounded-xl px-2">Comedy</li>
-            </ul>
-            <p className="text-[0.25em] mt-2 mr-3">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis
-              est suscipit at dolore exercitationem? A ipsam itaque, accusantium
-              explicabo ad veritatis quasi! Necessitatibus deserunt reiciendis
-              similique! Quia molestiae minus nihil!
-            </p>
+
+        <div className="absolute flex flex-col w-[95vw] ml-2 text-white top-[11rem] h-auto pb-3">
+          <div className="w-full h-full md:ml-[15%] md:mt-[-5%] md:flex">
+            <div className="hidden md:block w-[20%]">
+              <img
+                className="w-full h-auto object-cover"
+                src={`https://image.tmdb.org/t/p/original//${showModal?.poster_path}`}
+                alt=""
+              />
+            </div>
+            <div className="ml-5">
+              <h1 className="text-4xl font-bold bottom-[20%] w-[50%]">
+                {showModal.title}
+              </h1>
+              <ul className="mt-1 text-[0.5rem]">
+                <li className="border-2 inline rounded-xl px-2">Comedy</li>
+              </ul>
+              <p className="text-[0.25em] mt-2 mr-3 md:w-[40%]">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Veritatis est suscipit at dolore exercitationem? A ipsam itaque,
+                accusantium explicabo ad veritatis quasi! Necessitatibus
+                deserunt reiciendis similique! Quia molestiae minus nihil!
+              </p>
+            </div>
           </div>
 
-          <div className="mt-3">
+          <div className="mt-[4rem] relative">
             <h2 className="text-[0.7rem] font-semibold mb-3">Casts</h2>
             <ul className="w-full h-auto flex flex-wrap items-center gap-y-2">
               {casts.slice(0, 7).map((cast, id) => (
                 <li key={id} className="">
-                  <div className="w-[5em] min-h-[120px] flex flex-col justify-start items-center">
+                  <div className="w-[5em] h-[120px] flex flex-col items-center">
                     <img
                       src={`https://image.tmdb.org/t/p/original/${cast?.profile_path}`}
                       alt={cast?.name}
                       className="h-auto w-[4em]"
                     />
-                    <p className="text-[0.5rem] text-center mt-2">
+                    <p className="inline-block text-[0.5rem] text-center mt-2">
                       {cast?.name}
                     </p>
                   </div>
@@ -90,7 +100,7 @@ const MovieInfo = ({
               ))}
             </ul>
           </div>
-          <div className="my-4 max-w-[440px] h-full">
+          <div className="my-4 max-w-[640px] h-full">
             <h3 className="text-[0.7rem] font-semibold mb-3">Trailer</h3>
             <Player trailer={trailer} />
           </div>
